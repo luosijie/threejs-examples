0ff9bd169db0cd7ba2781f493e24c7115e65b089
Jesse Luo <luo_sj@163.com>
2018-01-03 16:44:30 +0800

    finaly

--- js/jump/main.js
+++ js/jump/main.js
@@ -1,14 +1,26 @@
-var game = new Game(showMask)
+var game = new Game(success,failed)
 game.init()
+game.addSuccessFn(success)
+game.addFailedFn(failed)
 
 var mask = document.querySelector('.mask')
 var restartButton = document.querySelector('.restart')
+var score = document.querySelector('.score')
+
 restartButton.addEventListener('click', restart) 
 
+// 游戏重新开始，执行函数
 function restart () {
 	mask.style.display = 'none'
 	game.restart()
 }
-function showMask(){
+// 游戏失败执行函数
+function failed(){
+	score.innerText = game.score
 	mask.style.display = 'flex'
+}
+// 游戏成功，更新分数
+function success (score) {
+	var scoreCurrent = document.querySelector('.score-current')
+	scoreCurrent.innerText = score
 }
