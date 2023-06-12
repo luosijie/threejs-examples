d302fcad53462acbd3abbd474e1ffa7b4992378f
luosijie <luosijie@jsunet.cn>
2023-06-12 10:17:02 +0800

    :heavy_minus_sign: delete projects not showing in homepage

--- src/pages/carton-room/config/resources.ts
+++
@@ -1,5 +0,0 @@
-import { LoaderType } from '@/utils/Loader'
-export default [
-    { name: 'modelRoom', type: LoaderType.GLTF, path: './resources/room.glb'},
-    { name: 'textureKda', type: LoaderType.Video, path: './resources/kda.mp4'}
-]
--- src/pages/carton-room/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import index from './index.vue'
-
-const app = createApp(index)
-app.mount(document.body)
